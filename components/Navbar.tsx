fd607cc96fd53b0186889cbc30e9354602436ad8
Shakya Karunathilake <46352201+stinger1999@users.noreply.github.com>
2022-08-15 23:25:13 +0530

    feat: added prettier to the project

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,7 @@
-import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import React from "react";
 import { GoogleLogin, GoogleLogout } from "react-google-login";
 import { AiOutlineLogout } from "react-icons/ai";
 import { BiSearch } from "react-icons/bi";
@@ -23,7 +23,7 @@ const Navbar = () => {
         </div>
       </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,3 @@
-
 import type { AppProps } from "next/app";
 import { useEffect, useState } from "react";
 
