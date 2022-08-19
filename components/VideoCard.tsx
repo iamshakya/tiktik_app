53f0760bab010bb712ecb02c4a3e1349147327b5
Shakya Karunathilake <46352201+stinger1999@users.noreply.github.com>
2022-08-19 22:52:03 +0530

    feat: video card component created

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -13,6 +13,22 @@ interface IProps {
 }
 
 const VideoCard: NextPage<IProps> = ({ post }) => {
+  const [isHover, setIsHover] = useState(false);
+  const [playing, setPlaying] = useState(false);
+  const [isVideoMuted, setIsVideoMuted] = useState(false);
+
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  const onVideoPress = () => {
+    if (playing) {
+      videoRef?.current?.pause();
+      setPlaying(false);
+    } else {
+      videoRef?.current?.play();
+      setPlaying(true);
+    }
+  };
+
   return (
     <div className="flex flex-col border-b-2 border-gray-200 pb-6">
       <div>
@@ -32,12 +48,56 @@ const VideoCard: NextPage<IProps> = ({ post }) => {
             </Link>
           </div>
           <div>
-            <Link href="">
-              <div>
-                <p>{post.postedBy.userName}</p>
+            <Link href="/">
+              <div className="flex items-center gap-2">
+                <p className="flex gap-2 items-center md:text-md font-bold text-primary">
+                  {post.postedBy.userName}{" "}
+                  <GoVerified className="text-blue-400 text-md" />
+                </p>
+                <p className="capitalize font-medium text-gray-500 hidden md:block">
+                  {post.postedBy.userName}
+                </p>
               </div>
             </Link>
           </div>
+        </div>
+      </div>
+      <div className="lg:ml-20 flex gap-4 relative">
+        <div
+          className="rounded-3xl"
+          onMouseEnter={() => setIsHover(true)}
+          onMouseLeave={() => setIsHover(false)}
+        >
+          <Link href="">
+            <video
+              ref={videoRef}
+              src={post.video.asset.url}
+              className="h-[300px] md:h-[400px] lg:h-[530px] w-[200px] lg:w-[600px] rounded-2xl cursor-pointer bg-gray-100"
+              loop
+            />
+          </Link>
+          {isHover && (
+            <div className="absolute bottom-6 cursor-pointer flex flex-row  w-[200px] lg:w-[600px] justify-around ">
+              {playing ? (
+                <button onClick={onVideoPress}>
+                  <BsFillPauseFill className="text-black text-2xl lg:text-4xl" />
+                </button>
+              ) : (
+                <button onClick={onVideoPress}>
+                  <BsFillPlayFill className="text-black text-2xl lg:text-4xl" />
+                </button>
+              )}
+              {isVideoMuted ? (
+                <button onClick={() => setIsVideoMuted(false)}>
+                  <HiVolumeOff className="text-black text-2xl lg:text-4xl" />
+                </button>
+              ) : (
+                <button onClick={() => setIsVideoMuted(true)}>
+                  <HiVolumeUp className="text-black text-2xl lg:text-4xl" />
+                </button>
+              )}
+            </div>
+          )}
         </div>
       </div>
     </div>
--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["www.collinsdictionary.com"],
+    domains: ["i0.wp.com"],
   },
 };
 
